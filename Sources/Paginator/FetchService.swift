1e73e00f19c2c7d7454fb6ae625f33d2886fe137
Dmytro Chapovskyi <dchapovskyi@shutterstock.com>
2023-01-30 16:25:30 +0100

    docs

--- Sources/Paginator/FetchService.swift
+++ Sources/Paginator/FetchService.swift
@@ -9,9 +9,23 @@ import Foundation
 
 public protocol FetchService {
 	
+	/**
+	 `Comparable` and `Identifiable` conformances are needed for `Paginator` to sort the items list after merge and resolve collisions.
+	 
+	 Comparable is to support sorting (by `>`). E.g., by date added / date updated / alphabetically by name etc.
+	 Both Comparable and Identifiable are needed to resolve duplicates of the same element.
+	 The element with the higher order is kept, and the one with the lower is discarded.
+	 */
 	associatedtype Item: Comparable & Identifiable
+	
+	/**
+	 An optional filter containing any conditions whatsoever - provided and handled by the `FetchService` implementation.
+	 */
 	associatedtype Filter
 	
+	/**
+	 The fetch request, pretty much self explanatory.
+	 */
 	func fetch(
 		count: Int,
 		page: Int,
