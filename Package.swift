53e8f82092702da81649ef012d6c78ca5e5d4e1d
Dmytro Chapovskyi <dc_dc@protonmail.com>
2023-07-04 11:32:08 +0200

    swift-tools-version decreased to 5.7

--- Package.swift
+++ Package.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 5.8
+// swift-tools-version: 5.7
 // The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
@@ -13,7 +13,7 @@ let package = Package(
             targets: ["SwiftPaginator"]),
     ],
     dependencies: [
-		.package(url: "https://github.com/dc-nrx/ReplaceableLogger.git", from: "0.1.0")
+		.package(url: "https://github.com/dc-nrx/ReplaceableLogger.git", from: "0.1.5")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
