170c20ec95b43359fd398acfb5d549ee193f66ff
Dmytro Chapovskyi <dc_dc@protonmail.com>
2023-12-19 12:00:47 +0100

    PaginatorForEach tmp commented

--- Sources/SwiftPaginator/PaginatorForEach.swift
+++ Sources/SwiftPaginator/PaginatorForEach.swift
@@ -1,49 +1,49 @@
+////
+////  PaginatorView.swift
+////  
+////
+////  Created by Dmytro Chapovskyi on 10.07.2023.
+////
 //
-//  PaginatorView.swift
-//  
+//import Foundation
+//import SwiftUI
 //
-//  Created by Dmytro Chapovskyi on 10.07.2023.
+///**
+// A
+// */
+//public struct PaginatorForEach<Item: Identifiable, Filter, Content: View>: View {
+//	
+//	@ObservedObject private var vm: PaginatorVM<Item, Filter>
+//	
+//	let content: (Item) -> Content
 //
-
-import Foundation
-import SwiftUI
-
-/**
- A
- */
-public struct PaginatorForEach<Item: Identifiable, Filter, Content: View>: View {
-	
-	@ObservedObject private var vm: PaginatorVM<Item, Filter>
-	
-	let content: (Item) -> Content
-
-	public init(
-		_ vm: PaginatorVM<Item, Filter>,
-		@ViewBuilder content: @escaping (Item) -> Content
-	) {
-		self.vm = vm
-		self.content = content
-	}
-	
-	public var body: some View {
-		ForEach(vm.items) { item in
-			content(item)
-				.onAppear {
-					vm.onItemShown(item)
-				}
-		}
-	}
-}
-
-#Preview {
-	let vm = Mocks.vm(prefetchDistance: 80)
-	
-	return List {
-		PaginatorForEach(vm) { item in
-			Text(item.name)
-		}
-	}
-	.onAppear {
-		vm.onAppear()
-	}
-}
+//	public init(
+//		_ vm: PaginatorVM<Item, Filter>,
+//		@ViewBuilder content: @escaping (Item) -> Content
+//	) {
+//		self.vm = vm
+//		self.content = content
+//	}
+//	
+//	public var body: some View {
+//		ForEach(vm.items) { item in
+//			content(item)
+//				.onAppear {
+//					vm.onItemShown(item)
+//				}
+//		}
+//	}
+//}
+//
+//#Preview {
+//	let vm = Mocks.vm(prefetchDistance: 80)
+//	
+//	return List {
+//		PaginatorForEach(vm) { item in
+//			Text(item.name)
+//		}
+//	}
+//	.onAppear {
+//		vm.onAppear()
+//	}
+//}
